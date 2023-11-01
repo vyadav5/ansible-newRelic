50a7e635c73101f3d2a22b1b723706812ad29580
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-01 18:19:37 +0530

    Update functions.groovy

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -1,1 +1,7 @@
+def cloneProjectRepo(repoUrl, branch) {
+    sh "git clone -b ${branch} ${repoUrl}"
+}
 
+def repoUrl = 'https://github.com/vyadav5/ansible-newRelic.git' 
+def branch = 'main'  
+cloneProjectRepo(repoUrl, branch)
